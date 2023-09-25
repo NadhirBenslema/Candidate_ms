356e3817153635a52252433d858f8e70d3d70da7
unknown <nadhir.benslema@esprit.tn>
2023-09-25 17:47:22 +0100

    eureka client configuration

--- src/main/java/com/example/ms_candidate/MsCandidateApplication.java
+++ src/main/java/com/example/ms_candidate/MsCandidateApplication.java
@@ -3,11 +3,13 @@ package com.example.ms_candidate;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsCandidateApplication {
 
     @Bean
